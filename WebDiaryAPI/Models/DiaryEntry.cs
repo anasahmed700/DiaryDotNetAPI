8b6c180d9d3b366317ae444896f0ec95f98b14c4
Anas Ahmed <anasahmed700@gmail.com>
2025-01-08 19:31:24 +0500

    Setting up project for DB access

---
+++ WebDiaryAPI/Models/DiaryEntry.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebDiaryAPI.Models
+{
+    public class DiaryEntry
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Please enter a title!")]
+        public string Title { get; set; } = string.Empty; 
+        [Required(ErrorMessage = "Please add some content!")]
+        public string Content { get; set; } 
+        [Required(ErrorMessage = "Please select a date!")]
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
